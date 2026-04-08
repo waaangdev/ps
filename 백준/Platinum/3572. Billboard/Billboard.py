681190f9e5e2e1ab2fc15faf32cc8faedff1c213
waaangdev <waaangdev@gmail.com>
2026-04-08 11:30:44 +0900

    [Platinum V] Title: Billboard, Time: 292 ms, Memory: 148448 KB -BaekjoonHub

--- 백준/Platinum/3572. Billboard/Billboard.py
+++ 백준/Platinum/3572. Billboard/Billboard.py
@@ -16,35 +16,28 @@ h,w,n =  list(map(int,sys.stdin.readline().split()))
 
 segtree = [[w]*(min(h,n)+1) for i in range(19)]
 
-def seg(l,r,idx,d):
-    d2 = (2**d)
-    if(idx*d2==l and r==l+d2):
-        return segtree[d][idx]
-    mid = idx*d2+d2//2
-    if(r<=mid):
-        return seg(l,r,idx*2,d-1)
-    elif(l>=mid):
-        return seg(l,r,idx*2+1,d-1)
-    else:
-        return max(seg(l,mid,idx*2,d-1),seg(mid,r,idx*2+1,d-1))
-
 for i in range(n):
     inp = int(sys.stdin.readline())
 
-    l,r = -1,min(h,n)-1
-    while l+1!=r:
-        mid = (l+r)//2
-        if(seg(0,mid+1,0,18)>=inp):
-            r = mid
+    idx = 0
+    for i in range(18,-1,-1):
+        if(idx*2 >= min(h,n)):
+            idx*=2
+            break
+        if(segtree[i][idx*2] >= inp):
+            idx = idx*2
         else:
-            l=mid
-    if(seg(0,r+1,0,18)>=inp):
-        sys.stdout.write(str(r+1)+"\n")
+            idx = idx*2+1
+        
+    r=idx
+    
+    if(idx < min(h,n)):
+        print(r+1)
         dum = r
         segtree[0][dum]-=inp
         for j in range(1,19):
             dum//=2
             segtree[j][dum]=max(segtree[j-1][dum*2],segtree[j-1][dum*2+1])
     else:
-        sys.stdout.write(str(-1)+"\n")
+        print(-1)
     #print(segtree)
