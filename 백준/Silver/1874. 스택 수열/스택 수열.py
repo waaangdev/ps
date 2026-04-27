6c1e156c9a8da070cb32b20ee549ad252c2f318f
waaangdev <waaangdev@gmail.com>
2026-04-27 23:07:22 +0900

    [Silver II] Title: 스택 수열, Time: 6100 ms, Memory: 259868 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+import random
+import math
+import heapq
+
+#ABCXXX - X
+
+#n = int(sys.stdin.readline())
+#li = list(map(int,sys.stdin.readline().split()))
+#st = sys.stdin.readline().strip()
+
+#sys.setrecursionlimit(100001)
+
+n = int(sys.stdin.readline())
+li = []
+st = deque()
+c = 1
+rl = ''
+for i in range(n):
+    inp = int(sys.stdin.readline())
+    if(c <= inp ):
+
+        while (c <= inp):
+            st.append(c)
+            rl+='+\n'
+            c+=1
+        rl+='-\n'
+        st.pop()
+    elif((st and st.pop() == inp)):
+        rl+='-\n'
+
+        pass
+    else:
+        rl = "NO"
+        break
+print(rl)
