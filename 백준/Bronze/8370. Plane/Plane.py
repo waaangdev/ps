9e0c0d093fdfd6159d27006602192fa9520f29eb
waaangdev <waaangdev@gmail.com>
2026-04-27 23:24:00 +0900

    [Bronze V] Title: Plane, Time: 124 ms, Memory: 112300 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.py
@@ -0,0 +1,15 @@
+import sys
+from collections import deque
+import random
+import math
+import heapq
+
+#ABCXXX - X
+
+#n = int(sys.stdin.readline())
+#li = list(map(int,sys.stdin.readline().split()))
+#st = sys.stdin.readline().strip()
+
+#sys.setrecursionlimit(100001)
+a,b,c,d = list(map(int,sys.stdin.readline().split()))
+print(a*b+c*d)
