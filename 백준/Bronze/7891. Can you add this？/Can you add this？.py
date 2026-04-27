8323f772780036a9a59e53fce7b31507812191c5
waaangdev <waaangdev@gmail.com>
2026-04-27 23:26:36 +0900

    [Bronze V] Title: Can you add this?, Time: 124 ms, Memory: 112296 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+import random
+import math
+import heapq
+
+#ABCXXX - X
+
+#n = int(sys.stdin.readline())
+#li = list(map(int,sys.stdin.readline().split()))
+#st = sys.stdin.readline().strip()
+
+#sys.setrecursionlimit(100001)
+
+for i in range(int(sys.stdin.readline())):
+    print(sum(list(map(int,sys.stdin.readline().split()))))
